fa65c16ffc4359898adf30ccd0f47d7b805f86d7
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-01-23 20:52:37 +0100

    řešení s použitím long

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Karel";
+        String surname = "Novák";
+        LocalDate birthdate = LocalDate.of(1970,3,12) ;
+        long numberOfContracts = 90;
+        long tunOfSeldCarot = 400;
+        String city = "Brno";
+        String licensePlate = "5B5 5555";
+        double consumptionPer100km = 5.6;
+        String IPAdress = "56.145.91.188";
+        long amountPerContract = tunOfSeldCarot/numberOfContracts;
+        System.out.println("Průměrné množství mrkve prodané na jednu smlouvu je "+amountPerContract+"t.");
     }
 }
